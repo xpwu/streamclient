354643b9d3b9697939acb9834e7933e622f803cc
xpwu <diodexp@foxmail.com>
2022-01-25 22:57:50 +0800

    add wxapp

---
+++ wxapp/stream/client.ts
@@ -0,0 +1,106 @@
+
+import {Request, Response, Status} from "./fakehttp";
+import {Net} from "./net"
+import {option, Option} from "./option"
+import {Millisecond} from "./duration"
+import {CloseEvent} from "./connection"
+
+
+export class Client {
+  private readonly net: Net;
+  private allReq: Map<number, (res: Response) => void>;
+  private reqId: number;
+  private onPush: (res:string)=>Promise<void> = (res:string)=>{return Promise.resolve()};
+  private onPeerClosed: ()=>Promise<void> = ()=>{return Promise.resolve()};
+  private op = new option
+
+  // ws or wss 协议。
+  constructor(wss: string, ...opf: Option[]) {
+    if (wss.indexOf("s://") == -1) {
+      wss = "ws://" + wss;
+    }
+
+    for (let o of opf) {
+      o(this.op)
+    }
+
+    this.net = new Net(wss, this.op.connectTimeout, {
+      onMessage: (value: string | ArrayBuffer): void => {
+        // 类型不对，直接返回
+        if (typeof value === "string") {
+          console.error("error response type(string): " + value);
+          return;
+        }
+
+        let res = new Response(value);
+        if (res.isPush()) {
+          // 异步执行
+          let _ = this.onPush(res.data())
+          return;
+        }
+
+        let clb = this.allReq.get(res.reqID()) || ((res: Response) => {});
+        this.net.receivedOneResponse()
+        clb(res);
+        this.allReq.delete(res.reqID());
+
+      }, onClose: (result: CloseEvent): void => {
+        this.allReq.forEach((value, key) => {
+          value(Response.fromError(key, new Error(result.reason)))
+        });
+        this.allReq.clear()
+
+        // 异步执行
+        let _ = this.onPeerClosed()
+      }
+    });
+
+    // start from 10
+    this.reqId = 10;
+    this.allReq = new Map();
+  }
+
+  public setPushCallback(clb :(res:string)=>Promise<void>) {
+    this.onPush = clb;
+  }
+
+  public setPeerClosedCallback(clb :()=>Promise<void>) {
+    this.onPeerClosed = clb;
+  }
+
+  public async Send(data: ArrayBuffer | string, header?: Map<string, string>)
+    : Promise<[string, Error | null]> {
+
+    let err = await this.net.Connect();
+    if (err != null) {
+      return ["", err];
+    }
+
+    let req = new Request(data, header);
+    let reqId = this.reqId++;
+    req.SetReqId(reqId);
+    err = await this.net.Write(req.ToData());
+    if (err != null) {
+      return ["", err];
+    }
+
+    return new Promise<[string, Error | null]>(
+      (resolve: (ret: [string, Error | null ]) => void, reject) => {
+        this.allReq.set(reqId, (res:Response)=>{
+          if (res.status != Status.Ok) {
+            resolve(["", new Error(res.data())]);
+            return
+          }
+
+          resolve([res.data(), null]);
+        });
+
+        setTimeout(()=>{
+          this.allReq.delete(reqId)
+          resolve(["", new Error("timeout")]);
+        }, this.op.requestTimeout/Millisecond);
+      })
+  }
+
+}
+
---
+++ wxapp/stream/net.ts
@@ -0,0 +1,119 @@
+import {Duration, Millisecond} from "./duration"
+import {Connection, MessageEvent, CloseEvent, Event} from "./connection"
+
+
+interface NetHandle {
+  onMessage(value: string | ArrayBuffer): void;
+
+  onClose(result: CloseEvent): void
+
+  onError?: () => void
+}
+
+export class Net {
+
+  private conn: Connection | null = null;
+  private connected: boolean = false;
+  private waitingConnect: Array<(ret: Error | null) => void> = new Array<(ret: Error | null) => void>();
+
+  constructor(private wss: string, private connectTimeout: Duration, private handle: NetHandle) {
+  }
+
+  private doWaitingConnect(err: Error | null) {
+    for (let waiting of this.waitingConnect) {
+      waiting(err)
+    }
+    this.waitingConnect = new Array<(ret: Error | null) => void>();
+  }
+
+  private invalidWebsocket() {
+    this.conn!.onmessage = () => {}
+    this.conn!.onopen = () => {}
+    this.conn!.onclose = () => {}
+    this.conn!.onerror = () => {}
+    this.conn = null;
+  }
+
+  public async Connect(): Promise<Error | null> {
+    if (this.connected) {
+      return null
+    }
+
+    return new Promise<Error | null>((resolve: (ret: Error | null) => void, reject) => {
+      this.waitingConnect.push(resolve);
+      if (this.conn != null) {
+        return
+      }
+
+      let timer = setTimeout(()=>{
+        // invalid this.websocket
+        this.invalidWebsocket()
+        this.connected = false;
+
+        this.doWaitingConnect(new Error("connect timeout"))
+      }, this.connectTimeout/Millisecond)
+
+      try {
+        this.conn = new Connection(this.wss);
+      }catch (e) {
+        this.conn = null;
+        this.connected = false;
+        clearTimeout(timer)
+        this.doWaitingConnect(new Error(e))
+        return
+      }
+
+      this.conn.onmessage = (result: MessageEvent)=>{
+        this.handle.onMessage(result.data)
+      };
+      this.conn.onopen = () => {
+        this.connected = true;
+        clearTimeout(timer)
+        this.doWaitingConnect(null);
+      };
+      this.conn.onclose = (result: CloseEvent) => {
+        console.info("onClosed, ", result.reason);
+        this.handle.onClose(result);
+        this.conn!.close();
+        this.conn = null;
+        this.connected = false;
+      };
+
+      this.conn.onerror = (result: Event) => {
+        console.error(result);
+        // 连接失败的防御性代码，websocket接口没有明确指出连接失败由哪个接口返回，故这里加上连接失败的处理
+        if (this.conn != null && !this.connected) {
+          clearTimeout(timer)
+          this.doWaitingConnect(new Error("websocket on error"));
+        }
+
+        if (!this.connected) {
+          return
+        }
+
+        if (this.handle.onError) {
+          this.handle.onError();
+        }
+
+        this.handle.onClose({code: -1, reason: "onerror", wasClean:true});
+        this.conn!.close();
+        this.conn = null;
+        this.connected = false;
+      };
+
+    });
+  }
+
+  public Write(data: ArrayBuffer): Error | null {
+    if (this.conn == null || !this.connected) {
+      return new Error("not connected")
+    }
+
+    return this.conn.send(data)
+  }
+
+  public receivedOneResponse():void {
+    this.conn?.receivedOneResponse()
+  }
+
+}
