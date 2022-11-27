74d169fe6d54ac1b17060ed62d53c03695476997
xpwu <diodexp@foxmail.com>
2022-11-27 20:48:41 +0800

    Update Config.java

--- android/app/src/androidTest/java/com/anywithyou/stream/Config.java
+++ android/app/src/androidTest/java/com/anywithyou/stream/Config.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Config {
-  public String Host = "127.0.0.1";
+  // 如果测试服在本机上，注意这里不能使用127.0.0.1 的方式，因为是启动一个模拟设备，与测试服电脑不是同一个设备
+  public String Host = "192.168.1.xxx";
   public int Port = 8080;
-  public Map<String, String> Headers = new HashMap<>();
+  public Map<String, String> Headers = new HashMap<String, String>() {{
+    put("api", "xxx");
+  }};
 
   static public Config Default = new Config();
 }
