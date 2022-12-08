793fab9554e84929f4f5d57b7e62e15d50a82683
xpwu <diodexp@foxmail.com>
2022-12-09 00:01:12 +0800

    Delete wxapp directory
    变更到 https://github.com/xpwu/ts-streamclient-wxapp

--- wxapp/stream/duration.ts
+++
@@ -1,9 +0,0 @@
-
-
-export type Duration = number
-
-export const Microsecond = 1
-export const Millisecond = 1000 * Microsecond
-export const Second = 1000 * Millisecond
-export const Minute = 60 * Second
-export const Hour = 60 * Minute
